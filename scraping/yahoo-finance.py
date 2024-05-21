d10ce2ce301c867890bb1ce9f724be81eb61490d
Ashi <144452098+ashi1072004@users.noreply.github.com>
2024-05-21 21:30:24 +0500

    saauhekjf

--- scraping/yahoo-finance.py
+++ scraping/yahoo-finance.py
@@ -6,36 +6,26 @@ import json
 url="https://finance.yahoo.com/sectors/technology/"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
-section1 = soup.find("section", class_=["svelte-ekgvwx"])
-section2 = soup.find("section", attrs={'data-testid' : 'investing-in'})
 main_list = []
 
-def subdata(tr):
-    sub_list = []
-    for td in tr:
-        sub_list.append(td.text.strip())
-    return sub_list
+def extract(element):
+    keys  = [th.text.strip() for th in element.find_all("th")]
+    values = [[td.text.strip() for td in tr.find_all("td")] for tr in element.tbody.find_all("tr")]
+    for value in values:
+        data = {keys[i]: value[i] for i in range(len(keys))}
+        main_list.append(data)
 
-main_list.append(section1.h2.text.strip())
-main_list.append(subdata(section1.find_all("th")))
-trs = section1.tbody.find_all("tr")
-for tr in trs:
-    main_list.append(subdata(tr.find_all("td")))
-
-
-main_list.append(section2.h2.text.strip())
-main_list.append(section2.p.text.strip())
-tables = section2.find_all("div", class_=["table-section-container"])
+# data.append(section1.h2.text.strip())
+extract(soup.find("section", class_=["svelte-ekgvwx"]))
+tables = soup.find_all("div", class_=["table-section-container"])
+# data.append(table.h3.text.strip())
 for table in tables:
-    main_list.append(table.h3.text.strip())
-    main_list.append(subdata(table.find_all("th")))
-    trs = table.tbody.find_all("tr")
-    for tr in trs:
-        main_list.append(subdata(tr.find_all("td")))
+    extract(table)
 
-# with open('yahoo-finance.json', 'w') as f:     
-#     json.dump(main_list, f, indent=4)
+# print(main_list)
 
-main_list_df = pd.DataFrame(main_list)
-print(main_list_df)
-main_list_df.to_csv('yahoo-finance.csv', quoting=1, index=False)
+with open('yahoo-finance.json', 'w') as f:     
+    json.dump(main_list, f, indent=4)
+
+data_df = pd.DataFrame(main_list)
+data_df.to_csv('yahoo-finance.csv', index=False)
