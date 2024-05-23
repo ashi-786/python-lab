9b72a51787a3633eb320c05815e0ec71aa1f1502
Ashi <144452098+ashi1072004@users.noreply.github.com>
2024-05-23 21:32:55 +0500

    kjsfu yur iyeur

---
+++ scraping/check_proxies.py
@@ -0,0 +1,43 @@
+import threading
+import queue
+import requests
+
+q = queue.Queue()
+valid = []
+
+with(open("proxy_list.txt", "r") as f):
+    proxies = f.read().split("\n")
+    for p in proxies:
+        q.put(p)
+
+def check():
+    global q
+    while not q.empty():
+        proxy = q.get()
+        try:
+            res = requests.get("http://ipinfo.io/json", proxies={"http": proxy, "https": proxy})
+        except:
+            continue
+        if res.status_code == 200:
+            print(proxy)
+
+for _ in range(10):
+    threading.Thread(target=check).start()
+
+# Another Method
+# import concurrent.futures
+
+# with open("proxy_list.txt", "r") as f:
+#     proxy = f.read().split("\n")
+#     proxies = [p for p in proxy]
+
+# def extract(proxy):
+#     try:
+#         r = requests.get("https://httpbin.org/ip", proxies={"http": proxy, "https": proxy}, timeout=2)
+#         print(r.json(), "ok")
+#     except:
+#         pass
+#     return proxy
+
+# with concurrent.futures.ThreadPoolExecutor() as exectr:
+#     exectr.map(extract, proxies)
---
+++ scraping/prayer-times.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+from time import sleep
+import json
+
+webcache = "https://webcache.googleusercontent.com/search?q=cache:"
+url="www.islamic-relief.org.uk/resources/prayer-timetables/"
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36']
+
+# scraper = cloudscraper.create_scraper()
+# r = scraper.get(url, headers=header)
+
+proxies = ['20.24.43.214:8080', '20.210.113.32:8080', '20.111.54.16:8080', '20.206.106.192:8080']
+p = choice(proxies)
+print(f"Using {p}")
+try:
+    r = requests.get(webcache+url, headers={'User-Agent': choice(user_agents)}, proxies={"http": f"http://{p}"})
+    # print(r.status_code)
+    soup = BeautifulSoup(r.content, "lxml")
+    a = soup.find("div", class_="c-listings__cards").find_all("a")
+    mkeys = [link.text.strip() for link in a]
+    # print(mkeys)
+    nkeys = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
+    links = [link.get('href').lstrip("https://") for link in a]
+    main_dict = {}
+    c = 0
+
+    for url1 in links:
+        pr = choice(proxies)
+        print(f"Using {pr}")
+        try:
+            sleep(3)
+            r = requests.get(webcache+url1, headers={'User-Agent': choice(user_agents)}, proxies={"http": f"http://{pr}"})
+            print(r.status_code)
+            soup = BeautifulSoup(r.content, "lxml")
+            trs = soup.find("tbody").find_all("tr")
+            # print(trs)
+            values = []
+            for tr in trs:
+                values.append(tr.find_all("td")[1].text.strip())
+            timing = {nkeys[i]: values[i] for i in range(len(nkeys))}
+            print(timing)
+            new = {mkeys[c]: timing}
+            main_dict.update(new)
+            # print(main_dict)
+        except:
+            new = {mkeys[c]: ""}
+            main_dict.update(new)
+            pass
+        finally:
+            c += 1
+    with open('prayer-times.json', 'w') as f:     
+        json.dump(main_dict, f, indent=4)
+except:
+    print("Failed")
+
+
