c7bf9c5a99bdf980c2d69846356e247417560423
Ashi <144452098+ashi1072004@users.noreply.github.com>
2024-05-25 23:09:28 +0500

    hdfiodyfoiwe

--- scraping/books_toscrape.py
+++ scraping/books_toscrape.py
@@ -20,35 +20,35 @@ with open("valid_proxies.txt", "r") as f:
 
 main_list =[]
 keys = ['Name', 'Img', 'Description', 'UPC', 'Product-Type', 'Price (excl. tax)', 'Price (incl. tax)', 'Tax', 'Availability', 'Number of reviews']
-for i in range(1, 51):
+# for i in range(1, 51):
+    # try:
+    #     sleep(1)
+p = choice(proxies)
+r = requests.get(base_url + f"page-{1}.html", headers={'User-Agent': choice(user_agents)}, proxies={"http": p})
+print(r.status_code)
+soup = BeautifulSoup(r.content, "lxml")
+articles = soup.find("ol", class_="row").find_all("article")
+for article in articles:
+    vals = []
+    src = base_url + article.find_all("a")[0].get('href')
     try:
         sleep(1)
         p = choice(proxies)
-        r = requests.get(base_url + f"page-{i}.html", headers={'User-Agent': choice(user_agents)}, proxies={"http": p})
+        r = requests.get(src, headers={'User-Agent': choice(user_agents)}, proxies={"http": p})
         print(r.status_code)
         soup = BeautifulSoup(r.content, "lxml")
-        articles = soup.find("ol", class_="row").find_all("article")
-        for article in articles:
-            vals = []
-            src = base_url + article.find_all("a")[0].get('href')
-            try:
-                sleep(1)
-                p = choice(proxies)
-                r = requests.get(src, headers={'User-Agent': choice(user_agents)}, proxies={"http": p})
-                print(r.status_code)
-                soup = BeautifulSoup(r.content, "lxml")
-                page = soup.find("article", class_="product_page")
-                vals.append(page.find("h1").text.strip())
-                vals.append(url + page.find("div", class_="thumbnail").find("img").get('src').lstrip("../.."))
-                vals.append(page.find("div", id="product_description").find_next("p").text.rstrip("...more"))
-                tds = page.find("table").find_all("td")
-                for td in tds:
-                    vals.append(td.text.strip())
-                main_list.append({keys[j]:vals[j] for j in range(len(keys))})
-            except:
-                pass
+        page = soup.find("article", class_="product_page")
+        vals.append(page.find("h1").text.strip())
+        vals.append(url + page.find("div", class_="thumbnail").find("img").get('src').lstrip("../.."))
+        vals.append(page.find("div", id="product_description").find_next("p").text.rstrip("...more"))
+        tds = page.find("table").find_all("td")
+        for td in tds:
+            vals.append(td.text.strip())
+        main_list.append({keys[j]:vals[j] for j in range(len(keys))})
     except:
         pass
+    # except:
+    #     pass
 
 with open("books_toscrape.json", "w") as f:
     json.dump(main_list, f, indent=4)
