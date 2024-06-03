d3e9a5880fba1cc557d58b675d699733340857e8
Ashi <144452098+ashi1072004@users.noreply.github.com>
2024-06-03 14:20:26 +0500

    sn oijllajdlk

--- scraping/Real-Estate-Listings.py
+++ scraping/Real-Estate-Listings.py
@@ -10,15 +10,15 @@ url = "https://www.point2homes.com/MX/Real-Estate-Listings/Quintana-Roo.html"
 r = requests.get(
     url='https://proxy.scrapeops.io/v1/',
     params={
-        'api_key': 'YOUR-API-KEY',
+        'api_key': 'API-KEY',
         'url': url+f"?page={1}",
         'bypass': 'cloudflare_level_1',
     },
 )
-# print(r.status_code)
+# # print(r.status_code)
 soup = bs(r.content, "html.parser")
 lis = soup.find("div", {'class': 'listings'}).find_all("div", {'class': 'item-cnt'})
-keys = ['title', 'price', 'price-drop', 'image-list', 'open-house-label', 'original-listing', 'Property-Summary', 'Property-Details', 'Description', 'Features', 'Price-History']
+keys = ['title', 'price', 'price-drop', 'image-list', 'open-house-label', 'original-listing', 'Property-Summary', 'Property-Details', 'Description', 'Features', 'Price-History', 'Agent-Details']
 main_list = []
 for li in lis:
     link = base_url + li.find("a", string="View Details").get('href')
@@ -42,26 +42,22 @@ for li in lis:
         try:
             price = main_content.find("div", class_="price").text.split("USD")[0].replace(",", "")
             vals.append(int(re.sub(r"[^\d]", "", price)))
-        except:
-            vals.append("")
+        except: vals.append("")
         # price-drop
         try:
             vals.append(main_content.find("span", class_="price-drop").text)
-        except:
-            vals.append("") # no price-drop
+        except: vals.append("") # no price-drop
         # img-list
         imgs = main_content.find("div", id="details-photos-slider").find_all("li")
         vals.append([img.find("img").get('src') for img in imgs])
         # open house label
         try:
             vals.append(main_content.find("div", class_="open-house-cnt").find("div", class_="open-house-right").text.strip())
-        except:
-            vals.append("")
+        except: vals.append("")
         # original-listing
         try:
             vals.append(main_content.find("a", string="Original Listing").get('href'))
-        except:
-            vals.append("")
+        except: vals.append("")
         # Property-Summary
         vals.append([li.text.strip() for li in main_content.find("div", class_="characteristics-cnt").find_all("li")])
         # Property-Details
@@ -71,16 +67,13 @@ for li in lis:
             for dl in details.find_all("dl"):
                 try:
                     chr_dict.update({dl.dt.text.strip() : dl.dd.text.strip()})
-                except:
-                    pass
+                except: pass
             vals.append(chr_dict)
-        except:
-            vals.append("")
+        except: vals.append("")
         # desc
         try:
             vals.append(main_content.find("div", class_="description-full-cnt").find("div", class_="description-text").text.strip())
-        except:
-            vals.append("")
+        except: vals.append("")
         # Features
         try:
             features = main_content.find("div", class_="features-list").find_all("div", class_="features-col")
@@ -90,8 +83,7 @@ for li in lis:
                 vals2 = [f.text.strip() for f in ftr.find_all("li")]
                 chr_dict.update({key : list(vals2)})
             vals.append(chr_dict)
-        except:
-            vals.append("")
+        except: vals.append("")
         # price-history
         try:
             hist_table = main_content.find("table", class_="price-history-tbl")
@@ -101,8 +93,35 @@ for li in lis:
                 tdList = [td.text.strip() for td in tr.find_all("td")]
                 history.append({thList[i]:tdList[i] for i in range(len(thList))})
             vals.append(history)
-        except:
-            vals.append("")# no history
+        except: vals.append("")# no history
+        
+        # Agent-Details
+        agents = soup2.find("aside", class_="content-side").find("div", class_="contacts_agents")
+        chr_dict = {}
+        try: # agent-photo
+            chr_dict.update({'agent-photo': agents.find("img", class_="profile").get('src')})
+        except: pass
+        try: #agent-name
+            chr_dict.update({'agent-name': agents.find("div", class_="agent-name").text.strip()})
+        except: pass
+        try: #detail
+            chr_dict.update({'agent-detail': agents.find("div", class_="agent-name").find_next("p").text.strip()})
+        except: pass
+        try: #company-logo
+            chr_dict.update({'company-logo': agents.find("img", class_="company-logo").get('src')})
+        except: pass
+        contacts = agents.find("ul", class_="agent-links")
+        try: #phone-list
+            phones = [link.get('data-phone') for link in contacts.find_all("span", class_="ic-phone")]
+            chr_dict.update({'phone-list': phones})
+        except: pass
+        try: #website
+            chr_dict.update({'website': contacts.find("a", class_="ic-website").get('href')})
+        except: pass
+        try: #profile-link
+            chr_dict.update({'profile-link': base_url+contacts.find("a", class_="ic-profile").get('href')})
+        except: pass
+        vals.append(chr_dict)
         # main list
         main_list.append({keys[i]:vals[i] for i in range(len(keys))})
     except:
