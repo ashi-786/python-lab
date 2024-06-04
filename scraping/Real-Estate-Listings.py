121884fdd6f1262dde963fc6e9ce156af90eab62
Ashi <144452098+ashi1072004@users.noreply.github.com>
2024-06-04 14:49:50 +0500

    ioewikwwkj

--- scraping/Real-Estate-Listings.py
+++ scraping/Real-Estate-Listings.py
@@ -1,34 +1,38 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import re
+import concurrent.futures
 import json
 
 base_url = "https://www.point2homes.com"
 url = "https://www.point2homes.com/MX/Real-Estate-Listings/Quintana-Roo.html"
-# Site has total 30 listings pages currently and every page has 24 Properties for sale
-# for i in range(1, 31):
-r = requests.get(
-    url='https://proxy.scrapeops.io/v1/',
-    params={
-        'api_key': 'API-KEY',
-        'url': url+f"?page={1}",
-        'bypass': 'cloudflare_level_1',
-    },
-)
-# # print(r.status_code)
-soup = bs(r.content, "html.parser")
-lis = soup.find("div", {'class': 'listings'}).find_all("div", {'class': 'item-cnt'})
-keys = ['title', 'price', 'price-drop', 'image-list', 'open-house-label', 'original-listing', 'Property-Summary', 'Property-Details', 'Description', 'Features', 'Price-History', 'Agent-Details']
+links = []
+keys = ['title', 'price', 'price-drop', 'Latitude', 'Longitude', 'image-list', 'open-house-label', 'original-listing', 'Property-Summary', 'Property-Details', 'Description', 'Features', 'Price-History', 'Agent-Details']
 main_list = []
-for li in lis:
-    link = base_url + li.find("a", string="View Details").get('href')
-    # print(f"\n{link}\n")
-    vals = []
+def get_links(page):
+    r = requests.get(
+        url='https://proxy.scrapeops.io/v1/',
+        params={
+            'api_key': 'API-KEY',
+            'url': url+f"?page={page}",
+            'bypass': 'cloudflare_level_1',
+        },
+    )
+    # print(r.status_code)
+    soup = bs(r.content, "html.parser")
+    lis = soup.find("div", {'class': 'listings'}).find_all("div", {'class': 'item-cnt'})
+    for li in lis:
+        links.append(base_url+li.find("a", string="View Details").get('href'))
+    
+
+def get_data(link):
     try:
+        # print(f"\nScraping {link}\n")
+        vals = []
         res = requests.get(
             url='https://proxy.scrapeops.io/v1/',
             params={
-                'api_key': 'f1bd630d-7af0-4123-b4dd-6c8010bed171',
+                'api_key': 'API-KEY',
                 'url': link,
                 'bypass': 'cloudflare_level_1',
             },
@@ -47,6 +51,14 @@ for li in lis:
         try:
             vals.append(main_content.find("span", class_="price-drop").text)
         except: vals.append("") # no price-drop
+        # Latitude
+        try:
+            vals.append(float(main_content.find('input', id=lambda value: value and value.startswith("Latitude_l")).get('value')))
+        except: vals.append("")
+        # Longitude
+        try:
+            vals.append(float(main_content.find('input', id=lambda value: value and value.startswith("Longitude_l")).get('value')))
+        except: vals.append("")
         # img-list
         imgs = main_content.find("div", id="details-photos-slider").find_all("li")
         vals.append([img.find("img").get('src') for img in imgs])
@@ -94,7 +106,7 @@ for li in lis:
                 history.append({thList[i]:tdList[i] for i in range(len(thList))})
             vals.append(history)
         except: vals.append("")# no history
-        
+
         # Agent-Details
         agents = soup2.find("aside", class_="content-side").find("div", class_="contacts_agents")
         chr_dict = {}
@@ -122,11 +134,22 @@ for li in lis:
             chr_dict.update({'profile-link': base_url+contacts.find("a", class_="ic-profile").get('href')})
         except: pass
         vals.append(chr_dict)
-        # main list
         main_list.append({keys[i]:vals[i] for i in range(len(keys))})
     except:
-        main_list.append({})
-        pass
+        main_list.append({"Error in": link})
+
+# Site has total 30 listings pages currently and every page has 24 Properties for sale
+# which is going to make 720 requests using third-party proxy agregator
+# Change the loop range as per the number of pages
+try:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(get_links, [i for i in range(1, 3)])
+except: pass
+# print(len(links))
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(get_data, links)
+# print(len(main_list))
 
 with open("Real-Estate-Listings.json", "w", encoding="utf-8") as f:
     json.dump(main_list, f, ensure_ascii=False, indent=4)
